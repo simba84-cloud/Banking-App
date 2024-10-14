d6f7b51e5631d1a2d4f946eaa67255b9cbb0c405
simba <simbamuzika@gmail.com>
2024-10-14 21:16:01 +0200

    create AccountService

---
+++ src/main/java/net/simbatools/banking/service/AccountService.java
@@ -0,0 +1,8 @@
+package net.simbatools.banking.service;
+
+import net.simbatools.banking.dto.AccountDto;
+
+
+public interface AccountService {
+    AccountDto createAccount(AccountDto accountDto );
+}
