c800005077dfab5e45162e59a55291b0b1c4dbfd
simba <simbamuzika@gmail.com>
2024-10-15 15:51:55 +0200

    refactor impl

--- src/main/java/net/simbatools/banking/service/AccountServiceImpl.java
+++ src/main/java/net/simbatools/banking/service/AccountServiceImpl.java
@@ -1,12 +1,14 @@
 package net.simbatools.banking.service;
 
 import net.simbatools.banking.dto.AccountDto;
+import net.simbatools.banking.entity.Account;
+import net.simbatools.banking.mepper.AccountMapper;
 import net.simbatools.banking.repository.AccountRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountServiceImpl implements AccountService{
-    private AccountRepository accountRepository;
+    private final AccountRepository accountRepository;
 
     public AccountServiceImpl(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
@@ -14,6 +16,8 @@ public class AccountServiceImpl implements AccountService{
 
     @Override
     public AccountDto createAccount(AccountDto accountDto) {
-        return null;
+        Account account = AccountMapper.mapToAccount(accountDto);
+        Account savedAcount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAcount);
     }
 }
