af4dea5bbd8f45a0540a71a4f30b737bdde5ba72
simba <simbamuzika@gmail.com>
2024-10-16 14:03:38 +0200

    add withdrawal endpoint

--- src/main/java/net/simbatools/banking/service/AccountService.java
+++ src/main/java/net/simbatools/banking/service/AccountService.java
@@ -2,9 +2,15 @@ package net.simbatools.banking.service;
 
 import net.simbatools.banking.dto.AccountDto;
 
+import java.util.List;
+
 
 public interface AccountService {
     AccountDto createAccount(AccountDto accountDto );
     AccountDto getAccountById(Long id);
     AccountDto deposit(Long id ,double amount);
+    AccountDto withdraw(Long id ,double amount);
+    List<AccountDto> getAllAccounts();
+
+
 }
