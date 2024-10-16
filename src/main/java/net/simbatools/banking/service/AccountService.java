c846ad38fc62ecc034dc4cf8209c0aa53b4c1ec0
simba <simbamuzika@gmail.com>
2024-10-16 10:59:14 +0200

    add deposit abstract

--- src/main/java/net/simbatools/banking/service/AccountService.java
+++ src/main/java/net/simbatools/banking/service/AccountService.java
@@ -5,4 +5,6 @@ import net.simbatools.banking.dto.AccountDto;
 
 public interface AccountService {
     AccountDto createAccount(AccountDto accountDto );
+    AccountDto getAccountById(Long id);
+    AccountDto deposit(Long id ,double amount);
 }
