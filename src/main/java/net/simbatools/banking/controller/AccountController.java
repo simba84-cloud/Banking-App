b2f93794afde3e7078185fa280811dbdeaa7f4f5
simba <simbamuzika@gmail.com>
2024-10-17 11:06:31 +0200

    delete endpoint

--- src/main/java/net/simbatools/banking/controller/AccountController.java
+++ src/main/java/net/simbatools/banking/controller/AccountController.java
@@ -53,4 +53,10 @@ public class AccountController {
         List <AccountDto> accounts = accountService.getAllAccounts();
         return ResponseEntity.ok(accounts);
     }
+   // delele acoount rest api
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAccount(@PathVariable Long id ){
+      accountService.deleteAccount(id);
+      return ResponseEntity.ok("Account is deleted successfully");
+    }
     }
